3c4ace2dc0d02f2e38f227bec81d1c10145872ea
Allana Larissa <larissaleonel98@gmail.com>
2023-03-27 16:26:57 -0300

    validates

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,13 +1,28 @@
-import  {Request, Response} from "express"
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import * as yup from 'yup'
 
-
-interface ICidade{
+interface ICidade {
     nome: string
+
 }
+const bodyvalidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3)
+})
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validatedData: ICidade | undefined = undefined;
+    try {
+        validatedData = await bodyvalidation.validate(req.body);
 
-export const create = (req: Request< {}, {}, ICidade>, res: Response)=>{
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
 
-    console.log(req.body.nome)
+        res.status(500).json({
+            erros: {
+                default: yupError.message,
+            }
+        })
+    }
 
-    return res.send('create')
+
 }
