795ba5ae6fba0fcd9b6771c0d3fc0db83bb6bc9e
Allana Larissa <larissaleonel98@gmail.com>
2023-03-27 13:53:10 -0300

    controller and models add

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { StatusCodes } from 'http-status-codes';
-const router =  Router();
+import { cidadesControllers } from '../controllers'
+
+const router = Router();
 
 router.get('/', (req, res) => {
-    return res.send('Olá, DEV!');
-  });
-  
-  router.post('/teste', (req, res) => {
+  return res.send('Olá, DEV!');
+});
 
-  
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-  });
-export{router}
+
+
+router.post('/cidades', cidadesControllers.create);
+
+
+export { router }
